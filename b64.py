49270d777efd9ea2b85db4f84154249425ca01bb
wojgadek <wojgadek@gmail.com>
2023-01-14 09:24:31 +0100

    Add files via upload
    code formatting

--- b64.py
+++ b64.py
@@ -1,11 +1,13 @@
 import base64
 
+
 def encrypt(text):
     encoded_text = base64.b64encode(text.encode())
     decoded_text = base64.b64decode(encoded_text).decode()
     return encoded_text.decode(), decoded_text
 
-text = "Hello World"
+
+text = "Test działania funkcji"
 encoded_text, decoded_text = encrypt(text)
 print("Encrypted text: ", encoded_text)
 print("Decrypted text: ", decoded_text)
